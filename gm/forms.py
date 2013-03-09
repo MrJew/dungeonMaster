132ea6184037e3ceb340a3b2bc0ddbe779812db0
Sparksmith <ivivanchev@gmail.com>
2013-03-09 16:50:07 -0500

    Wtf 2.0 -> after delete parts of gm/form.py

---
+++ gm/forms.py
@@ -0,0 +1,15 @@
+from gm.models import *
+from django.forms import *
+from django.forms import *
+from django import forms
+from gm.models import *
+
+class QuestForm(forms.ModelForm):
+    title = forms.CharField()
+    snippet = forms.CharField()
+    xp = forms.IntegerField()
+    choice = forms.ChoiceField(User.objects.all())
+
+#class Meta:
+ #   model = Quest
+  #  fields = ("title", "snippet", "xp", "players")
