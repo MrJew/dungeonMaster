09c773c3e77fdc9576ff32f4e80fb1c5be932f8c
DanielKirov <kirov.daniel@gmail.com>
2013-03-09 22:06:36 +0000

    Dobavena forma + na4alno view

--- gm/views.py
+++ gm/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+from django.template import Context, RequestContext
+from django.shortcuts import render_to_response
+from gm.forms import QuestForm
+
+def createQuest(request):
+    if request.method=='POST':
+        form = QuestForm(request.POST)
+        if form.is_valid():
+            quest = form.save()
+        else:
+            pass
+    else:
+        form = QuestForm()
+
