404b3cf995bd653974d0679735d1d15db5907af7
MrJew <6hope9@mail.bg>
2013-03-10 14:11:36 +0000

    facade folder created

--- system/views.py
+++ system/views.py
@@ -1,54 +1,9 @@
 # Create your views here.
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from character.models import *
-import string, operator
-from random import randint
+
 
 def main(request):
     print "Tva e viewto na maina"
     context = RequestContext(request)
     return render_to_response('system/main.html',context)
-
-def rpn(s):
-    ops = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.div}
-    while True:
-        st = []
-        for tk in string.split(s):
-            print tk
-            if tk in ops:
-                print "operator"
-                y,x = st.pop(),st.pop()
-                z = ops[tk](x,y)
-            else:
-                print "str"
-                z = int(tk)
-            st.append(z)
-        assert len(st)<=1
-        if len(st)==1:
-            print(st.pop())
-            break
-
-def getValues(c):
-    s = Stats.objects.get(character=c)
-    values = {'agi': s.getAgi(),
-              'str': s.getStr(),
-              'int': s.getInt(),
-              'dex': s.getDex(),
-              'vit': s.getVit(),
-              'speed': s.getSpeed(),
-              'beauty': s.getBeauty(),
-              'lvl':s.xp/1000,
-              'dice': randint(1,6)}
-    return values
-
-def formToString(s,v):
-    st = string.split(s)
-    final=""
-    for i in st:
-        if len(i)>2:
-            final+=str(v[i])
-        else:
-            final+=i
-        final+=" "
-    return final
